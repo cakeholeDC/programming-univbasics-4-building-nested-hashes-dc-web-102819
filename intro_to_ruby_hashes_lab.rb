6edc71f6563331ee1cb207ce3b2b16d07884857e
Kyle Cole <kylepcole@gmail.com>
2019-10-15 23:26:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,10 +34,18 @@ def monopoly_with_third_tier
     railroads: {
       pieces: 4,
       names: {
-        reading_railroad: {},
-        pennsylvania_railroad: {},
-        b_and_o_railroad: {},
-        shorline_railroad: {}
+        reading_railroad: {
+        mortgage_value: "$100"
+        },
+        pennsylvania_railroad: {
+          mortgage_value: "$100"
+        },
+        b_and_o_railroad: {
+          mortgage_value: "$100"
+        },
+        shorline_railroad: {
+          mortgage_value: "$100"
+        }
       },
       rent_in_dollars: {
         one_piece_owned: 25,
